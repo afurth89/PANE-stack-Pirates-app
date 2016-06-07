7ef2d6a0a866123f46bb1a1139e292d90497ca4d
Andrew Furth <andrew.furth@gmail.com>
2016-06-07 10:47:45 -0700

    POST route successfully adds pirate to db

--- server/routes/pirates.js
+++ server/routes/pirates.js
@@ -2,15 +2,27 @@ var express = require('express'),
     router = express.Router(),
     knex = require('../db/knex');
 
-router.get('/', (req, res) => {
-  knex('pirates').select()
-    .then(function(pirates) {
-      res.send(pirates);
-    })
-    .catch(function(err) {
-      res.send(err)
-    })
-})
+router.route('/')
+  .get((req, res) => {
+    knex('pirates').select()
+      .then(function(pirates) {
+        res.send(pirates);
+      })
+      .catch(function(err) {
+        res.send(err);
+      })
+  })
+  .post((req, res) => {
+    knex('pirates').insert(req.body.pirate)
+      .returning('*')
+      .then(function(pirate) {
+        console.log("SUCCESSFUL POST: ", pirate)
+        res.send(pirate);
+      })
+      .catch(function(err) {
+        res.send(err);
+      })
+  })
 
 // GET '/:id'
 // PUT '/:id'
