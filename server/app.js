e60e24825ae50e138fc286df58ba9b4efa2f5703
Andrew Furth <andrew.furth@gmail.com>
2016-06-07 09:57:08 -0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,9 @@
+// TODO:
+
+// 1. Get database working, figure out routing to communicate 
+//    to database & perform CRUD
+// 2. On client side, wire up Angular and connect view for showing pirates
+// 3. Handle creation of pirate
+// 4. Handle deletion
+// 5. Handle update
+// 6. Refactor!
---
+++ server/knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "pirates_development"
+    }
+  }
+}
